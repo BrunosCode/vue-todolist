8cd48f4697a77bc34bc3b9035e67776f6bcf4ec2
brunEdo <edobruno.m@gmail.com>
2021-09-20 15:17:22 +0200

    add task and clean input

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
+class Task {
+    constructor( titole, dueDate ) {
+        this.titole = titole;
+        this.dueDate = dueDate;
+        this.state = "toDo";
+    }
+}
+
 const app = new Vue (
     {
         el: "#root",
         data: {
-
+            tasks: [],
+            newTitole: "",
+            newDueDate: ""
         },
         methods: {
-
+            addTask: function(titole, dueDate) {
+                this.tasks.push(new Task(titole, dueDate));
+            },
+            changeState: function(taskIndex, newState) {
+                this.tasks[taskIndex].state = newState;
+            },
+            removeTask: function(taskIndex) {
+                this.tasks.splice(taskIndex, 1);
+            },
+            resetInput: function() {
+                this.newTitole = "";
+                this.newDueDate = "";
+            }
         }
     }
 );
